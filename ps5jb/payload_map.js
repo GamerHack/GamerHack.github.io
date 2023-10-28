c7e8e371ab31d3099fa110eb92988051bdcb6740
GamerHack <76641485+GamerHack@users.noreply.github.com>
2023-10-28 19:47:18 -0400

    Add files via upload

---
+++ ps5jb/payload_map.js
@@ -0,0 +1,69 @@
+const payload_map =
+    [
+        {
+            displayTitle: 'K-Stuff',
+            description: 'FW 4.03 & 4.50 Only. FPKG enabler',
+            fileName: 'ps5-kstuff-v1.1.bin',
+            author: 'sleirsgoevy',
+            source: 'https://github.com/sleirsgoevy/ps4jb2/blob/ps5-403/ps5-kstuff.bin',
+            version: '1.1'
+        },
+        {
+            displayTitle: 'libhijacker game-patch',
+            description: '(v1.124) Patches supported games to run at higher framerates, and adds debug menus to certain titles.',
+            fileName: 'libhijacker-spawner-1.124.elf;libhijacker-daemon-1.124.elf',
+            author: 'illusion0001, astrelsky',
+            source: 'https://github.com/illusion0001/libhijacker/releases',
+            loader: 'libhijacker',
+            version: '1.124'
+        },
+        {
+            displayTitle: 'PS5 Payload ELF Loader',
+            description: 'Uses port 9021. Persistent network elf loader',
+            fileName: 'elfldr.elf',
+            author: 'john-tornblom',
+            source:'https://github.com/john-tornblom/ps5-payload-elfldr/releases',
+            version: '0.5'
+        },
+        {
+            displayTitle: 'FTPS5 (Non-Persistent)',
+            description: 'FTP Server',
+            fileName: 'ftps5-np.elf',
+            author: 'SiSTR0, zecoxao, EchoStretch',
+            source:'https://github.com/EchoStretch/FTPS5/releases',
+            version: '1.4'
+        },
+        {
+            displayTitle: 'FTPS5 (Persistent)',
+            description: 'FTP Server',
+            fileName: 'ftps5-p.elf',
+            author: 'SiSTR0, zecoxao, EchoStretch',
+            source:'https://github.com/EchoStretch/FTPS5/releases',
+            version: '1.4'
+        },
+        {
+            displayTitle: 'Versions',
+            description: 'Shows kernel build, os and sdk versions',
+            fileName: 'versions.elf',
+            author: '?',
+            source:'https://mega.nz/folder/4xA2ATTI#6lHvye1JUfhxRGvqOva8fw/folder/gwhWmRrB',
+            version: '1.0'
+        },
+        {
+            displayTitle: 'GetOSVersion',
+            description: 'very slow',
+            fileName: 'getOsVersion.elf',
+            author: 'todo',
+            source:'https://mega.nz/folder/4xA2ATTI#6lHvye1JUfhxRGvqOva8fw/folder/gwhWmRrB',
+            version: '1.0'
+        },
+        {
+            displayTitle: 'PS5_Browser_appCache_remove',
+            description: 'Removes appcache from browser.',
+            fileName: 'Browser_appCache_remove.elf',
+            author: 'Storm21CH',
+            source:'https://github.com/Storm21CH/PS5_Browser_appCache_remove/blob/main/Browser_appCache_remove.elf',
+            version: '1.0fix'
+        }
+
+    ];
