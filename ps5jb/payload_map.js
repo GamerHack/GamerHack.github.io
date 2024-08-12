12cb4e01962e8ec2762a1c64cdb96f02a0dff7ec
GamerHack <76641485+GamerHack@users.noreply.github.com>
2024-08-12 18:11:39 -0400

    Added etaHEN-1.8B

--- ps5jb/payload_map.js
+++ ps5jb/payload_map.js
@@ -1,12 +1,12 @@
 const payload_map =
     [
         {
-            displayTitle: 'etaHEN 1.7B By LM',
+            displayTitle: 'etaHEN 1.8B By LM',
             description: 'Runs With 3.xx and 4.xx. FPKG enabler For FW 4.03-4.51 Only.',
-            fileName: 'etaHEN-1.7B.bin',
+            fileName: 'etaHEN-1.8B.bin',
             author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
             source: 'https://github.com/LightningMods/etaHEN',
-            version: '1.7 beta'
+            version: '1.8 beta'
         },
         {
             displayTitle: 'K-Stuff',
