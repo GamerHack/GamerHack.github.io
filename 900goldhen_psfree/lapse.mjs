5c1dc849435661b3092155f63b482cae1fefd672
GamerHack <76641485+GamerHack@users.noreply.github.com>
2025-07-27 09:11:49 -0400

    Re-added GoldHEN v2.4b18.4

--- 900goldhen_psfree/lapse.mjs
+++ 900goldhen_psfree/lapse.mjs
@@ -1606,7 +1606,7 @@ async function patch_kernel(kbase, kmem, p_ucred, restore_info) {
     localStorage.ExploitLoaded="yes"
     sessionStorage.ExploitLoaded="yes";
    //alert("kernel exploit succeeded!");
-    msgs.innerHTML = "GoldHEN v2.4b18.5 Loaded ...";
+    msgs.innerHTML = "GoldHEN v2.4b18.4 Loaded ...";
 }
 
 
@@ -1832,7 +1832,7 @@ var loader_addr = chain.sysp(
 
  var req = new XMLHttpRequest();
  req.responseType = "arraybuffer";
- req.open('GET','goldhen_2.4b18.5.bin');
+ req.open('GET','goldhen_2.4b18.4.bin');
  req.send();
  req.onreadystatechange = function () {
   if (req.readyState == 4) {
