6b730fd6d2159d0552c0fa9c765f3256df2b052e
GamerHack <76641485+GamerHack@users.noreply.github.com>
2025-08-21 19:48:00 -0400

    Lapse AIO fix payload implemented

--- 900goldhen_psfree/lapse.mjs
+++ 900goldhen_psfree/lapse.mjs
@@ -139,6 +139,11 @@ const leak_len = 16;
 const num_leaks = 5;
 const num_clobbers = 8;
 
+//Payload_Loader
+ const PROT_READ = 1;
+ const PROT_WRITE = 2;
+ const PROT_EXEC = 4;
+
 let chain = null;
 var nogc = [];
 
@@ -1606,7 +1611,6 @@ async function patch_kernel(kbase, kmem, p_ucred, restore_info) {
     localStorage.ExploitLoaded="yes"
     sessionStorage.ExploitLoaded="yes";
    //alert("kernel exploit succeeded!");
-    msgs.innerHTML = "GoldHEN v2.4b18.5 Loaded ...";
 }
 
 
@@ -1811,13 +1815,9 @@ function array_from_address(addr, size) {
     return og_array;
 }
 
-kexploit().then(() => {
-
- const PROT_READ = 1;
- const PROT_WRITE = 2;
- const PROT_EXEC = 4;
-
-var loader_addr = chain.sysp(
+function PayloadLoader(Pfile)
+{
+    var loader_addr = chain.sysp(
   'mmap',
   new Int(0, 0),                         
   0x1000,                               
@@ -1832,7 +1832,7 @@ var loader_addr = chain.sysp(
 
  var req = new XMLHttpRequest();
  req.responseType = "arraybuffer";
- req.open('GET','goldhen_2.4b18.5.bin');
+ req.open('GET',Pfile);
  req.send();
  req.onreadystatechange = function () {
   if (req.readyState == 4) {
@@ -1856,5 +1856,17 @@ var loader_addr = chain.sysp(
     );	
    }
  };
+
+
+}
+
+kexploit().then(() => {
+
+//Load ABC fix as a regular Payload
+setTimeout(PayloadLoader("aio_patches.bin"),500);
+log("AIO Fixes Applied.!");
+//Load GoldHEN :)
+setTimeout(PayloadLoader("goldhen_2.4b18.5.bin"),500);
+msgs.innerHTML = "GoldHEN v2.4b18.5 Loaded ...";
 
 })
