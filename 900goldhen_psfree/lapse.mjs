9d454d8e5414bea1db630495f77813f22e0b0f90
GamerHack <76641485+GamerHack@users.noreply.github.com>
2025-09-02 15:05:31 -0400

    Added GoldHEN v2.4b18.6

--- 900goldhen_psfree/lapse.mjs
+++ 900goldhen_psfree/lapse.mjs
@@ -1711,8 +1711,8 @@ export async function kexploit() {
     
      if (localStorage.ExploitLoaded === "yes" && sessionStorage.ExploitLoaded!="yes") {
 		   setTimeout(PayloadLoader("aio_patches.bin"),500);
-		   setTimeout(PayloadLoader("goldhen_2.4b18.5.bin"),500);
-		   msgs.innerHTML = "GoldHEN v2.4b18.5 Loaded ...";
+		   setTimeout(PayloadLoader("goldhen_2.4b18.6.bin"),500);
+		   msgs.innerHTML = "GoldHEN v2.4b18.6 Loaded ...";
 		   return new Promise(() => {});
       }
  
@@ -1868,7 +1868,7 @@ kexploit().then(() => {
 setTimeout(PayloadLoader("aio_patches.bin"),500);
 log("AIO Fixes Applied.!");
 //Load GoldHEN :)
-setTimeout(PayloadLoader("goldhen_2.4b18.5.bin"),500);
-msgs.innerHTML = "GoldHEN v2.4b18.5 Loaded ...";
+setTimeout(PayloadLoader("goldhen_2.4b18.6.bin"),500);
+msgs.innerHTML = "GoldHEN v2.4b18.6 Loaded ...";
 
 })
